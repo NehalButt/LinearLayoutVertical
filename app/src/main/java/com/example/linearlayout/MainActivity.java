59a0982e624bd141b83444e3927616a47e89b548
White_Devil <pashanehal8@gmail.com>
2021-12-02 13:10:00 -0800

    haha lol

---
+++ app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+EditText name,email,address,password,confirmpassword;
+Button register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        address=findViewById(R.id.address);
+        password=findViewById(R.id.password);
+        confirmpassword=findViewById(R.id.confirmpassword);
+        register=findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
